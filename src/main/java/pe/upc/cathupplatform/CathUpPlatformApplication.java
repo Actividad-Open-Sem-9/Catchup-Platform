f0e75adbf4097b216b336b50ac7fc71da8bfb891
Al3xiel <gabriel280104casas@gmail.com>
2024-10-10 20:11:59 -0500

    chore: setup project.

---
+++ src/main/java/pe/upc/cathupplatform/CathUpPlatformApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.cathupplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathUpPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CathUpPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/upc/cathupplatform/CathUpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.cathupplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CathUpPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
